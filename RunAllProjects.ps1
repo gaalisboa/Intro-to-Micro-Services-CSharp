dd01d91d1acc3ce8c1fe15dcb1f26715a7d88fbe
Gabriel de Paula Souza <gaahlisboa@gmail.com>
2022-12-23 21:29:53 -0300

    bugs fixed, good to go

---
+++ RunAllProjects.ps1
@@ -0,0 +1,3 @@
+start powershell {dotnet watch run --project .\FrontEnd\GeekShopping.Web\GeekShopping.Web.csproj}
+start powershell {dotnet watch run --project .\Services\GeekShopping.IdentityServer\GeekShopping.IdentityServer.csproj}
+start powershell {dotnet watch run --project .\Services\GeekShopping.ProductAPI\GeekShopping.ProductAPI.csproj}
