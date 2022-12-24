b0990b72d628e1d564864aa97a4c6bff99dd14f6
Gabriel de Paula Souza <gaahlisboa@gmail.com>
2022-12-23 21:41:49 -0300

    that's the script

--- RunAllProjects.ps1
+++ RunAllProjects.ps1
@@ -1,3 +1,5 @@
+# Script for running multiple projects in VSCode
+
 start powershell {dotnet watch run --project .\FrontEnd\GeekShopping.Web\GeekShopping.Web.csproj}
 start powershell {dotnet watch run --project .\Services\GeekShopping.IdentityServer\GeekShopping.IdentityServer.csproj}
 start powershell {dotnet watch run --project .\Services\GeekShopping.ProductAPI\GeekShopping.ProductAPI.csproj}
